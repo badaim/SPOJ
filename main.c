9b3494962f030abbcb50da1564828813330041e6
Krzysztof Srokowski <krzysztof.srokowski@scalac.io>
2017-05-28 16:38:38 +0200

    KC001 - Dodawanie liczb calkowitych

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+    {
+        int first;
+        int second;
+        int third;
+        scanf("%d",&first);
+        scanf("%d",&second);
+        scanf("%d",&third);
+        printf("%d",(first+second+third));
+        return 0;
+
+    }
